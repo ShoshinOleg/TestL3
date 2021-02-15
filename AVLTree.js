778c0a1d4da88d9a79e6aa82ed0cc4895b108ea1
ShoshinOleg <shoshin1997@rambler.ru>
2021-02-15 21:58:24 +0300

    add simple insert

--- AVLTree.js
+++ AVLTree.js
@@ -1,8 +1,34 @@
 
 
+class AVLTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.rigth = null;
+        this.height = 0;
+    }
+}
+
 class AVLTree {
     constructor() {
-        
+        this.root = null;
+    }
+
+    insert(value) {
+        this.root = this.putNode(this.root, value);
+    }
+
+    putNode(node, value) {
+        if(!node) {
+            return new AVLTreeNode(value);
+        }
+        if(value < node.value) {
+            node.left = this.putNode(node.left, value);
+        } 
+        else if (value > node.value) {
+            node.right = this.putNode(node.right, value);
+        }
+        return node;
     }
 }
 
--- tests/test.js
+++ tests/test.js
@@ -2,14 +2,29 @@ const t = require('tap');
 const AVLTree = require('../AVLTree');
 
 t.test('AVLTree', (t) => {
+    let tree;
+    t.beforeEach((done, t) => {
+        tree = new AVLTree();
+        done();
+    });
 
     t.test('Tree exists', (t) => {
-        const tree = new AVLTree();
-
         t.ok(tree, 'Tree exists');
         t.end();
     });
 
+    t.test('Insert', (t) => {
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
+
+        t.same(tree.root.value, 10, 'Check insert in root');
+        t.same(tree.root.left.value, 5, 'Check insert in left');
+        t.same(tree.root.right.value, 15, 'Check insert in right');
+
+        t.end();
+    });
+    
     t.end();
 });
 
